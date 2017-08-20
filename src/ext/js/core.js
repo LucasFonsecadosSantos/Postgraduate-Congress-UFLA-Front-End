d5e30d1af7c825de5f366877529bfeb051741c2d
Lucas Fonseca dos Santos <lucas@lcfcompany.com.br>
2017-08-20 21:34:18 +0000

    Last fixies

--- src/ext/js/core.js
+++ src/ext/js/core.js
@@ -10,6 +10,16 @@ $(document).ready(function(){
       }
   })
 })
+var $root = $('html, body');
+$('a').click(function() {
+    var href = $.attr(this, 'href');
+    $root.animate({
+        scrollTop: $(href).offset().top
+    }, 500, function () {
+        window.location.hash = href;
+    });
+    return false;
+});
 
 // $(document).ready(function(){       
 //    var scroll_start = 0;
